b891283c46cdb84685b8a38da96ed40eee2faacf
duggalr <duggalr42@gmail.com>
2024-11-21 12:05:52 -0800

    testing main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,22 +73,6 @@ export default function Home() {
             </main>
 
         </>
-       
-//         <main>
-//             {/* Top Banner */}
-//             <div className="bg-gradient-to-r from-purple-700 to-purple-900 text-white py-1 shadow-lg">
-//                 <p className="text-sm text-center">
-//                     Made with ❤️ by <a href="https://lambdalabs.com/" target="_blank" rel="noopener noreferrer"
-// className="text-blue-400 hover:text-blue-600 hover:no-underline transition-colors duration-300">
-//     Lambda
-// </a>
-//                 </p>
-//             </div>
-
-//             <HeroNavBar userAuthenticated={userContext?.isAuthenticated} pageLoading={loadingRef.current} />
-//             <HeroPrimary userAuthenticated={userContext?.isAuthenticated} />
-
-//         </main>
 
     );
 
