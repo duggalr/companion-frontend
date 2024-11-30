beb18c62abbb7550b9fd3509f12239c8df5d7914
duggalr <duggalr42@gmail.com>
2024-11-30 14:00:59 -0500

    fix unused bug

--- src/lib/utils/generateAnonUserId.js
+++ src/lib/utils/generateAnonUserId.js
@@ -3,7 +3,7 @@
     // abstract and replace in associated places
 
 export default function generateUserID(){
-    return new Promise(function(resolve, reject){
+    return new Promise(function(resolve){
         let user_id = Math.floor(Math.random() * 1000000000000000000).toString();
         resolve(user_id);
     });
