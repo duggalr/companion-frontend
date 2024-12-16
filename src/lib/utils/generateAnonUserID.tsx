fea4f7d06d8f44c785dc6c1e63e467add844d619
duggalr <duggalr42@gmail.com>
2024-12-16 13:00:56 -0500

    update to remove typescript

--- src/lib/utils/generateAnonUserID.tsx
+++ src/lib/utils/generateAnonUserID.tsx
@@ -1,7 +1,7 @@
 export default function generateAnonUserID(): Promise<string>{
 
     return new Promise(function(resolve){
-        let user_id = Math.floor(Math.random() * 1000000000000000000).toString();
+        const user_id = Math.floor(Math.random() * 1000000000000000000).toString();
         resolve(user_id);
     });
 
--- src/lib/utils/handleAPIFetch.js
+++ src/lib/utils/handleAPIFetch.js
@@ -1,13 +1,15 @@
-type DataDict = {
-    [key: string]: any;
-}
+// type DataDict = {
+//     [key: string]: string;
+// }
 
-export default async function handleAPIFetch(
-    endpoint_url: string,
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-    access_token: string | null,
-    data: DataDict | null
-): Promise<any | null> {
+// export default async function handleAPIFetch(
+//     endpoint_url: string,
+//     method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+//     access_token: string | null,
+//     data: DataDict | null
+// ): Promise<any | null> {
+
+export default async function handleAPIFetch() {
 
     try {
 
