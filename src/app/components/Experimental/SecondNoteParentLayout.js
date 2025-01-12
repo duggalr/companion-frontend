8dd9ba03c73c9a71a312c057da1ed575ff2a88e6
duggalr <duggalr42@gmail.com>
2025-01-12 15:24:04 -0500

    parent layout update

--- src/app/components/Experimental/SecondNoteParentLayout.js
+++ src/app/components/Experimental/SecondNoteParentLayout.js
@@ -7,55 +7,158 @@ import Markdown from 'react-markdown';
 
 const SecondNoteParentLayout = ({chapterDict, noteDict}) => {
 
-    const [currentText, setCurrentText] = useState('');
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [isTyping, setIsTyping] = useState(false);
+    const tmp_list = [
+        {
+            'note': {
+                // "title": "What is a List?",
+                "description": "## What is a List?\n\nSo, **what is a list?** Think of a list like a container where you can keep a bunch of items in a specific order. So, if you're building a card game like poker, you might want to keep track of a deck of cards, and the order of those cards matters.\n\n\n### **Key Points:**\n- A list is just a collection of things that are ordered (so the order *counts*! 👀).\n- Lists let you group multiple items into one variable (like a deck of cards).\n- In Python, we define a list using square brackets `[]`."
+            },
+            'example': {
+                'description': "#Below is a list with 4 elements.\n#You can run the code by clicking the Run Button on the top right.\n\nlist_one = [1, 2, 3, 4]\nprint(list_one)\n"
+            },
+            'try_exercise': {
+                'description': "Try creating your own list on the IDE to the right with 3 numbers of your choice and print it out. Remember, use square brackets and separate the items with commas."
+            }
+        },
+    ];
 
+    // const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentNoteText, setCurrentNoteText] = useState('');
+
+    // const [currentTyping, setCurrentTyping] = useState(false);
+    const typingEffectStarted = useRef(false);
+
+    // const [exampleIndex, setExampleIndex] = useState(0);
+    const [currentExampleText, setCurrentExampleText] = useState('');
+    const [currentTryExerciseText, setCurrentTryExerciseText] = useState('');
+
+    const [showExampleButton, setShowExampleButton] = useState(false);
     const [showExample, setShowExample] = useState(false);
-    const [currentExampleDict, setCurrentExampleDict] = useState({});
-    const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
-    let tmp_list = [
-        {
-            "title": "What is a List?",
-            "description": "So, **what is a list?** Think of a list like a container where you can keep a bunch of items in a specific order. So, if you're building a card game like poker, you might want to keep track of a deck of cards, and the order of those cards matters. Here's an example..",
-            "key_points": [
-                "A list is just a collection of things that are ordered (so the order *counts*! 👀).",
-                "Lists let you group multiple items into one variable (like a deck of cards).",
-                "In Python, we define a list using square brackets `[]`."
-            ]
-        },
-        {
-            // 'example': "#The below is a list with 4 elements.\nlist_one = [1, 2, 3, 4]\nprint(list_one)\n",
-            'description': "#The below is a list with 4 elements.\nlist_one = [1, 2, 3, 4]\nprint(list_one)\n",
-        },
-        // {
-        //     "try_it": "Try creating your own list on the IDE to the right with 3 numbers of your choice and print it out. Remember, use square brackets and separate the items with commas."
-        // }
-    ]
+    const [showTryExerciseButton, setShowTryExerciseButton] = useState(false);
+    const [showSubmitExerciseButton, setShowSubmitExerciseButton] = useState(false);
 
-    const createTypewriterEffect = (text) => {
+    const [consoleOutput, setConsoleOutput] = useState('Console Output');
 
-        let timeout;
-        for (let idx = 0; idx <= text.length-1; idx++){
-            let current_ch = text[idx];
-            setCurrentText(previousText => previousText + current_ch);
+
+    const _createTypewriterEffect = (text, set_text_fn, current_index, text_type) => {
+        if (current_index < text.length) {
+
+            // Delay the typing effect by 20ms
+            setTimeout(() => {
+
+                console.log('text_type', text_type);
+
+                // Append the next character from text
+                set_text_fn(previousText => previousText + text[current_index]);
+    
+                // Recursive call with the next index
+                _createTypewriterEffect(text, set_text_fn, current_index + 1, text_type);  
+                
+            }, 10);
+
+        } else {
+
+            if (text_type === 'show_example_button'){
+                // TODO: 
+                setShowExampleButton(true);
+            } else if (text_type === 'show_try_exercise_button'){
+                // TODO:
+                setShowTryExerciseButton(true);
+            }
+
         }
+    };
+    
+    const currentModuleIndex = useRef(0);
+    const currentModuleDict = useRef({});
+
+    const _showNote = async () => {
+
+        let note_element = currentModuleDict.current['note'];
+
+        _createTypewriterEffect(
+            note_element.description,
+            setCurrentNoteText, 
+            0, 
+            'show_example_button'
+        );
 
     };
 
-    const _handleNoteSequenceShow = async () => {
+    const _showExample = async () => {
 
-        createTypewriterEffect(tmp_list[0].description);
+        let example_element = currentModuleDict.current['example'];
+        _createTypewriterEffect(
+            example_element.description,
+            setCurrentExampleText,
+            0,
+            null
+        );
+
+    };
+
+    const _showTryExercise = async () => {
+
+        let try_exercise_element = currentModuleDict.current['try_exercise'];
+        _createTypewriterEffect(
+            try_exercise_element.description,
+            setCurrentTryExerciseText,
+            0,
+            'show_try_exercise_button'
+        );
 
     }
 
-    // TODO: typewriting effect
-    useEffect(() => {
+    const handleShowExampleClick = async () => {
+        setShowExampleButton(false);
+        setShowExample(true);
+        _showExample();
+        
+        // let example_element = tmp_list[1];
+        // _createTypewriterEffect(example_element.description, setExampleText, 'show_try_exercise_button');
+    };
 
-        _handleNoteSequenceShow();
+    const handleShowTryExerciseClick = async () => {
+        setShowExampleButton(false);
+        setShowExample(false);
+        setShowTryExerciseButton(false);
+
+        // show submit exercise button
+        setShowSubmitExerciseButton(true);
+        _showTryExercise();
+    };
+
+
+    const handleRunButtonClick = async () => {
+        setConsoleOutput('>>> example run...')
+    }
+
+
+    useEffect(() => {
+        if (tmp_list && tmp_list.length > 0) {
+        
+            currentModuleIndex.current = 0;
+            currentModuleDict.current = tmp_list[0];
+    
+            if (typingEffectStarted.current === false){
+                typingEffectStarted.current = true;
+                _showNote();
+            }
+
+        }
+
+        // console.log('current-typing:', typingEffectStarted.current);
+        // if (typingEffectStarted.current === false){
+        //     typingEffectStarted.current = true;
+        //     if (tmp_list && tmp_list.length > 0) {
+        //         let first_element = tmp_list[0];
+        //         _createTypewriterEffect(first_element.description, setCurrentText, 0, 'show_example_button');
+        //     }
+        // }
 
     }, []);
+
 
     return (
 
@@ -71,63 +174,64 @@ const SecondNoteParentLayout = ({chapterDict, noteDict}) => {
 
                     <div className="space-y-4">
                         <div>
-                            <h3 className="text-gray-900 font-medium">
+                            <h3 className="text-gray-900 font-bold">
                                 {noteDict.title}
                             </h3>
                             <p className="text-[15px] tracking-normal leading-9 pt-4">
                                 {/* <TypeWriter text={noteDict.description} /> */}
-                                {currentText}
+                                {/* <Markdown> */}
+                                    {currentNoteText}
+                                {/* </Markdown> */}
                             </p>
-                            {/* <h3 className="text-gray-900 font-medium pt-5 pb-4">
-                                Key Points:
-                            </h3>
-                            <ul className="list-disc space-y-4">
 
-                                {noteDict.key_points.map((item, index) => (
-                                    <li className="text-[15px] ml-3 " key={index}>
-                                        {item}
-                                    </li>
-                                ))}
-
-                            </ul> */}
-
-                            {/* <p>
-                                Key Points:
-                            </p> */}
-                            {/* TODO: */}
-                            {/* Key Points */}
-                            {/* Try it */}
-
-                            {/* <p className="text-gray-600 text-[15.5px] tracking-normal leading-9 pt-4">
-                                {noteDict.description}
-                            </p> */}
                         </div>
                     </div>
 
                     {/* Buttons */}
-                    {/* <div className="mt-6 text-center space-x-2">
-                        <button
-                            type="button"
-                            className="py-3 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        >
-                            <FontAwesomeIcon icon={faQuestion} className="pr-1" />
-                            Ask a Question
-                        </button>
+                    <div className="mt-6 text-center space-x-2">
 
-                        <button
-                            type="button"
-                            className="py-3 px-5 me-2 mb-2 text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                        >
-                            Challenge Me
-                            <FontAwesomeIcon icon={faArrowRight} className="pl-1" />
-                        </button>
-                    </div> */}
+                        {/* Example Button */}
+                        {(showExampleButton === true) && (
+                            <button
+                                type="button"
+                                className="py-3 px-5 me-2 mb-2 text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                onClick={handleShowExampleClick}
+                            >
+                                Show Example
+                                <FontAwesomeIcon icon={faArrowRight} className="pl-1" />
+                            </button>
+                        )}
+
+                        {/* Try Exercise Button */}
+                        {(showTryExerciseButton === true) && (
+                            <button
+                                type="button"
+                                className="py-3 px-5 me-2 mb-2 text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                onClick={handleShowTryExerciseClick}
+                            >
+                                Challenge
+                                <FontAwesomeIcon icon={faArrowRight} className="pl-1" />
+                            </button>
+                        )}
+
+                        {/* Submit Button */}
+                        {(showSubmitExerciseButton === true) && (
+                            <button
+                                type="button"
+                                className="py-3 px-5 me-2 mb-2 text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            >
+                                Submit
+                                <FontAwesomeIcon icon={faArrowRight} className="pl-1" />
+                            </button>
+                        )}
+                        
+                    </div>
 
                 </div>
 
 
                 {/* Second Half */}
-                {(showExample === true) ? (
+                {(showExample === true) && (
 
                     <div className="w-1/2 ml-0 flex flex-col pt-0 border-l-2 border-gray-50">
 
@@ -135,22 +239,25 @@ const SecondNoteParentLayout = ({chapterDict, noteDict}) => {
                                 
                                 <div className="flex justify-between">
                                     <h2 className="text-lg font-semibold text-gray-800 mb-4">Code</h2>
-                                    <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-[13.5px] px-3 py-0 me-0 mb-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                    <button
+                                        type="button"
+                                        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-[13.5px] px-3 py-0 me-0 mb-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                                        onClick={handleRunButtonClick}
+                                    >
                                         <FontAwesomeIcon icon={faPlay} className="pl-1 pr-1 text-[12px]"/>{" "}Run
                                     </button>
                                 </div>
                                 
                                 <textarea
-                                        className="w-full h-64 resize-none rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none p-2 font-mono text-sm text-gray-800"
-                                        placeholder="# Write your code here..."
-                                    >
-                                        {currentText}
-                                    </textarea>
+                                    className="w-full h-64 resize-none rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none p-2 font-mono text-sm text-gray-800"
+                                    placeholder="# Write your code here..."
+                                    value={currentExampleText}
+                                ></textarea>
 
                                 <div className="flex-grow p-0 pt-4">
                                     <h2 className="text-lg font-semibold text-gray-800 mb-4">Console Output</h2>
                                     <div className="h-40 overflow-auto bg-black text-green-500 p-2 rounded-md font-mono text-sm">
-                                    Console output will appear here...
+                                    {consoleOutput}
                                     </div>
                                 </div>
 
@@ -158,14 +265,9 @@ const SecondNoteParentLayout = ({chapterDict, noteDict}) => {
 
                         </div>
 
-                    ) : (
-
-                        null 
-
                     )}
 
                 </div>
-
 
             </div>
 
